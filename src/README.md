5bff0c400c17d754abcf3d09ab241ad207144bca
Maneesh Singh <mann.biher@yahoo.co.in>
2020-11-18 11:37:32 -0500

    Basic graph output

---
+++ src/README.md
@@ -0,0 +1,16 @@
+# Instructions for running the code
+
+## Install dependencies
+```
+pip install -r requirements.txt
+```
+
+## Run main.py
+```
+python main.py
+```
+
+## Run Tests
+```
+python run_tests.py
+```
--- src/out_plot.py
+++ src/out_plot.py
@@ -1,11 +1,36 @@
 """Creates output plots."""
-
-def dummy_mu_data():
-    pass
-    causal_conf = [[
-        97,99.4
-    ]]
+import matplotlib.pyplot as plt
 
 
-def dummy_tn_data():
-    pass
+def plot_for_mu(avg_causal_by_mu, avg_purity_by_mu):
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 4), dpi=90)
+    ax = axes[0]
+    for mu, causal in avg_causal_by_mu.items():
+        ax.plot(causal)
+    ax.set_xlabel('Iteration')
+    ax.set_ylabel('Average Causality Confidence')
+
+    ax = axes[1]
+    for mu, purity in avg_purity_by_mu.items():
+        ax.plot(purity)
+    ax.set_xlabel('Iteration')
+    ax.set_ylabel('Average Purity')
+    # plt.show()
+    plt.savefig('mu_graph.png')
+
+
+def plot_for_tn(avg_causal_by_tn, avg_purity_by_tn):
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 4), dpi=90)
+    ax = axes[0]
+    for mu, causal in avg_causal_by_tn.items():
+        ax.plot(causal)
+    ax.set_xlabel('Iteration')
+    ax.set_ylabel('Average Causality Confidence')
+
+    ax = axes[1]
+    for mu, purity in avg_purity_by_tn.items():
+        ax.plot(purity)
+    ax.set_xlabel('Iteration')
+    ax.set_ylabel('Average Purity')
+    # plt.show()
+    plt.savefig('tn_graph.png')
