d11aa33644a0c5f26951f41fa412deab5ba1e924
Maneesh Singh <mann.biher@yahoo.co.in>
2020-11-27 16:33:33 -0500

    Add terraform for ec2 run

---
+++ infrastructure/ec2/vars.tf
@@ -0,0 +1,12 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+}
+
+variable "ec2_key" {
+  description = "EC2 key pair"
+}
+
+variable "spot_price" {
+  description = "EC2 spot price"
+}
+
