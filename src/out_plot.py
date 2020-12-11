226edaf575e6fd7b632937ab740c9eb6e7d16bd7
Maneesh Singh <mann.biher@yahoo.co.in>
2020-12-11 04:51:27 -0500

    Clean graph

--- src/out_plot.py
+++ src/out_plot.py
@@ -3,24 +3,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_causal_purity(avg_causal, avg_purity, iterations, outfile):
+def plot_causal_purity(avg_causal, avg_purity, iterations, outfile, label_prefix):
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 4))
     ax = axes[0]
-    for mu, causal in avg_causal.items():
-        ax.plot(causal)
+    for value, causal in avg_causal.items():
+        ax.plot(causal, label=f'{label_prefix}{value}', marker='+')
     ax.set_xlabel('Iteration')
     # ax.set_xticks(np.arange(1,7))
     ax.set_yticks(np.arange(95.5, 100.5, .5))
     ax.set_ylabel('Average Causality Confidence')
 
     ax = axes[1]
-    for mu, purity in avg_purity.items():
-        ax.plot(purity)
+    for value, purity in avg_purity.items():
+        ax.plot(purity, label=f'{label_prefix}{value}')
     ax.set_xlabel('Iteration')
     ax.set_ylabel('Average Purity')
     # ax.set_xticks(np.arange(1, 7))
     ax.set_yticks(np.arange(0, 125, 20))
     # plt.show()
+    plt.legend(bbox_to_anchor=(1, 0.5), loc='center left', ncol=1)
     plt.savefig(outfile)
 
 
@@ -29,7 +30,8 @@ def plot_for_mu(avg_causal_by_mu, avg_purity_by_mu, iterations=5):
         avg_causal_by_mu,
         avg_purity_by_mu,
         iterations,
-        'mu_graph.png')
+        'mu_graph.png',
+        '$\mu$=')
 
 
 def plot_for_tn(avg_causal_by_tn, avg_purity_by_tn, iterations=5):
@@ -37,4 +39,5 @@ def plot_for_tn(avg_causal_by_tn, avg_purity_by_tn, iterations=5):
         avg_causal_by_tn,
         avg_purity_by_tn,
         iterations,
-        'tn_graph.png')
+        'tn_graph.png',
+        'tn=')
